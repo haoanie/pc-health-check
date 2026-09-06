d0a7ce3790fc1f328cb7b3ee24cd022366e1a553
haoanie <wx136ghgk@outlook.com>
2026-09-06 16:41:07 +0800

    Add App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+
+namespace TiJianWinUI
+{
+    public partial class App : Application
+    {
+        private Window? _window;
+        public App()
+        {
+            InitializeComponent();
+            UnhandledException += (s, e) => { };
+        }
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            _window = new MainWindow();
+            _window.Activate();
+        }
+    }
+}
